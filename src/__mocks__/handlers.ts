38874d39c32179470fed3b852202c3b19e0d16e2
Blue-Kite <bluelemon112@gmail.com>
2025-02-06 00:46:34 +0900

    feat: msw handler 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,11 +6,44 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const mockApiHandlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const data = (await request.json()) as Event;
+    const { id: _, ...eventData } = data;
+    return HttpResponse.json(
+      {
+        event: {
+          eventData,
+          id: String(events.length + 1),
+        },
+      },
+      { status: 201 }
+    );
+  }),
 
-  http.put('/api/events/:id', async () => {}),
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const updatedEvent = (await request.json()) as Event;
+    const eventIndex = events.findIndex((event) => event.id === params.id);
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    if (eventIndex === -1) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    events[eventIndex] = updatedEvent;
+    return HttpResponse.json({ event: updatedEvent });
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const eventIndex = events.findIndex((event) => event.id === params.id);
+
+    if (eventIndex === -1) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    events.splice(eventIndex, 1);
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
